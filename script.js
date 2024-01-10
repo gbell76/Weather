70da63cd1ca03ccaf229eabe7ed91ab383f580d8
Gina Sciacchitano <gbell76767@gmail.com>
2024-01-09 19:14:48 -0500

    current weather info

--- script.js
+++ script.js
@@ -3,39 +3,54 @@ const input = document.querySelector('#input')
 const previousSearches = localStorage.getItem("searches")
 const previous = document.querySelector('#previous')
 const cityName = document.querySelector('#cityname')
+const icon = document.querySelector('#icon')
 const weather = document.querySelector('#weather')
 const temperature = document.querySelector('#temperature')
+const wind = document.querySelector('#wind')
 const humidity = document.querySelector('#humidity')
+const forecast = document.querySelector('#forecast')
 const key = '1340be71494f66ddb1b4ff4f454d3b17'
 
-const searchPrevious = async(btn) =>{
-    const geocode = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${btn.textContent}&limit=1&appid=${key}`)
+const searchPrevious = async(btn) => {
+    let geocode;
+    if(input.value){
+        geocode = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${btn.value}&limit=1&appid=${key}`)
+        cityName.textContent = btn.value + ' ' + dayjs().format('dddd, MMMM D, YYYY')
+    }else{
+        geocode = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${btn.textContent}&limit=1&appid=${key}`)
+        cityName.textContent = btn.textContent + ' ' + dayjs().format('dddd, MMMM D, YYYY')
+    }
     const geoData = await geocode.json()
     const lat = geoData[0].lat
     const lon = geoData[0].lon
     const weatherResponse = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${key}&units=imperial`)
     const weatherData = await weatherResponse.json()
-    console.log(weatherData.weather[0].main)
-    console.log(weatherData.main.temp)
-    cityName.textContent = btn.textContent
+    icon.src = 'http://openweathermap.org/img/w/' + weatherData.weather[0].icon + '.png'
     weather.textContent = 'Weather: ' + weatherData.weather[0].main
     temperature.textContent = 'Temperature: ' + weatherData.main.temp
+    wind.textContent = 'Wind Speed: ' + weatherData.wind.speed + 'mph'
     humidity.textContent = 'Humidity: ' + weatherData.main.humidity
+    while(forecast.firstChild){
+        forecast.removeChild(forecast.firstChild)
+    }
+    //forecast api call
+    input.value = ""
 }
 
 const search = async() => {
     if(input.value){
-        //console.log(input.value)
+        searchPrevious(input)
         if(previousSearches){
             const searches = previousSearches.split(',')
-            searches.push(input.value)
-            localStorage.setItem('searches', searches.join(','))
+            if(!searches.includes(input.value)){
+                searches.push(input.value)
+                localStorage.setItem('searches', searches.join(','))
+            }
         }else{
             const searches = []
             searches.push(input.value)
             localStorage.setItem('searches', searches.join(','))
         }
-        input.value = ""
         while(previous.firstChild){
             previous.removeChild(previous.firstChild)
         }
